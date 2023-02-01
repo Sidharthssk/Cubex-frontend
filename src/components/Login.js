374cd954b609eb6c0be194bd2a3cf6371f82abdf
Sidharthssk <sidharthssk.18@gmail.com>
2023-02-01 12:40:53 +0530

    Made login components

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,7 +18,7 @@ function Login() {
   return (
     <form className="container px-5 py-3 login d-flex flex-column position-absolute top-50 start-50 translate-middle"
         onSubmit={handleSubmit}>
-        <input type="text" placeholder="Email" className="my-4" id="email" />
+        <input type="text" placeholder="Email" className="my-4" id="email" autoComplete="off"/>
         <input type="password" placeholder="Password" className="my-3" id="password" />
         <button className="btn btn-primary my-3" type="submit">Login</button>
     </form>
